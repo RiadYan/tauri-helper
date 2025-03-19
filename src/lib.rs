03f0516631f61d623ec90c55f93c238ea69f0bca
RiadYan <182303059+RiadYan@users.noreply.github.com>
2025-03-19 14:16:59 +0100

    some checks and debugs

--- src/lib.rs
+++ src/lib.rs
@@ -100,19 +100,13 @@ pub use tauri_helper_macros::*;
 ///
 /// If the function encounters an error during file generation, it will log the error and exit the
 /// build process with a non-zero status code.
-pub fn generate_command_file(options: TauriHelperOptions) {
+pub fn generate_command_file() {
     let workspace_root = find_workspace_dir(Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()));
-    build_print::println!("Workspace Root Dir: {}", &workspace_root.display());
-    //todo: make commands_dir in TauriHelperOptions.
-
-    let commands_dir = workspace_root.join("target").join("tauri_commands_list");
-    build_print::println!("commands_dir Dir: {}", &commands_dir.display());
-
+    let commands_dir = workspace_root.join("target").join("commands");
     fs::create_dir_all(&commands_dir).unwrap();
 
+    // Read the workspace members from `Cargo.toml`
     let workspace_members = get_workspace_members(&workspace_root);
-    build_print::println!("workspace members: {:#?}", &workspace_members);
-
     for member in &workspace_members {
         println!("cargo:rerun-if-changed={}", member);
     }
@@ -135,29 +129,10 @@ pub fn generate_command_file(options: TauriHelperOptions) {
                     if let Ok(ast) = parse_file(&content) {
                         for item in ast.items {
                             if let syn::Item::Fn(func) = item {
-                                let mut is_tauri_command = false;
-                                let mut is_auto_collect = false;
-
-                                // Check for #[tauri::command] and #[auto_collect_command]
                                 for attr in &func.attrs {
-                                    if attr.path().is_ident("tauri")
-                                        && attr.path().segments.len() > 1
-                                        && attr.path().segments[1].ident == "command"
-                                    {
-                                        is_tauri_command = true;
-                                    }
                                     if attr.path().is_ident("auto_collect_command") {
-                                        is_auto_collect = true;
+                                        functions.push(func.sig.ident.to_string());
                                     }
-                                }
-
-                                // Collect the function based on the options
-                                if options.collect_all && is_tauri_command {
-                                    // Collect all #[tauri::command] functions
-                                    functions.push(func.sig.ident.to_string());
-                                } else if is_tauri_command && is_auto_collect {
-                                    // Collect only functions with both #[tauri::command] and #[auto_collect_command]
-                                    functions.push(func.sig.ident.to_string());
                                 }
                             }
                         }
@@ -165,14 +140,12 @@ pub fn generate_command_file(options: TauriHelperOptions) {
                 }
             }
         }
-
         let package_name = get_workspace().package.name.replace("-", "_");
-
+        // Write to the crate's command file
         let command_file = commands_dir.join(format!("{}.txt", crate_name));
         let mut file = File::create(&command_file).unwrap();
 
         for func in functions {
-            // Due to some Tauri shenanigans and some of my laziness and also because some options are only available in nightly.
             if crate_name.replace("-", "_") == "src_tauri" {
                 let full_name = format!("{}::{}", package_name, func);
                 println!("found: {:#?}", &full_name);
